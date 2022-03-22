9e1c22d39e0d9e11aedc4178faec427e65bc4a02
Sergey Kabanov <sergeykabanov86@gmail.com>
2022-03-22 13:49:26 +0200

    Добавьте файлы проекта.

---
+++ CV19Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19Wpf.ViewModels
+{
+    public class MainView
+    {
+
+        #region Properties
+
+        public string Title { get; } = "Main CV19 Title";
+
+
+        #endregion Properties
+
+
+
+        #region Commands
+
+
+
+
+        #endregion Commands
+
+
+
+        #region Constructors
+
+        public MainView()
+        {
+        }
+
+
+
+        #endregion Constructors
+
+
+
+        #region Methods
+
+
+
+
+        #endregion Methods
+
+    }
+}
